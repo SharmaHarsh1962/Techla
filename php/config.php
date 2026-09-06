897cd3502a2f85f764ee3b09c1a42dc8affae9c4
SharmaHarsh1962 <sharmasarita0669@gmail.com>
2026-09-06 18:37:07 +0545

    api updated

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-define('NVIDIA_API_KEY', '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');
+define('NVIDIA_API_KEY', 'xxxxx');
 define('NVIDIA_API_URL', 'https://api.groq.com/openai/v1/chat/completions');
 define('NVIDIA_MODEL', 'openai/gpt-oss-120b');
 ?>
